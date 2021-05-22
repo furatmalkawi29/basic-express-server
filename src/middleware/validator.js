38241bba0dba0c64663d16d64e9c8f0921cb522c
furatmalkawi29 <malkawifurat29@gmail.com>
2021-05-22 23:48:35 +0300

    done-dev2

--- src/middleware/validator.js
+++ src/middleware/validator.js
@@ -1,5 +1,16 @@
 'use strict';
 
+/**
+ * This function is a middleware tha accepts HTTP request and response,
+ * and next method. It checks if there's a name
+property in request's query and throws an error if not 
+ * 
+ * @param {object} err HTTP request object
+ * @param {object} req HTTP request object
+ * @param {object} res HTTP response object
+ * @param {function} next Function that calls the next middleware
+ */ 
+
 module.exports = (req,res,next) =>{
   if( req.query.name ) { next(); } // Run the next middleware
   else { throw new Error('No name found');}
